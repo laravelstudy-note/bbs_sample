0c33f02f101d0fc83ee0e92f9de912d1d4cb86d6
laravelstudy-note <ryosukemiyazawa+laravelstudy@gmail.com>
2020-06-09 00:48:40 +0900

    不足していたファイルを追加

--- laravel_bbs_sample/routes/web.php
+++ laravel_bbs_sample/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "BbsEntryController@index");
+Route::post('/create', "BbsEntryController@create");
